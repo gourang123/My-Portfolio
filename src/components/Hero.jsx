0594a1009fd19f75ddd3be3835a62002accc8fb5
gourang123 <gourangsharma1998@gmail.com>
2023-10-14 15:24:30 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,86 @@
+import { motion } from "framer-motion";
+import { TypeAnimation } from "react-type-animation";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <ComputersCanvas />
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className="col-span-4 place-self-center mt-4 lg:mt-0"
+      ></motion.div>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="flex flex-col  text-center sm:text-left justify-center"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={["Gourang Sharma", 1000, "Web Developer", 1000]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <div>
+            <Link
+              to="Contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-pink-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              to="./assets/RESUME.pdf"
+              target="_blank"
+              download="Resume.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        ></motion.div>
+      </div>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,240 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  meta,
+  Tata,
+  bikaner,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  cpp,
+  nextjs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Next.JS Developer",
+    icon: backend,
+  },
+  {
+    title: "Backend Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "C++",
+    icon: cpp,
+  },
+  {
+    name: "NextJs",
+    icon: nextjs,
+  },
+];
+
+const experiences = [
+  {
+    title: "Floor Manager",
+    company_name: "Tata Motors",
+    icon: Tata,
+    iconBg: "#383E56",
+    date: "October 2019 - November 2020",
+    points: [
+      "Guided and supervised service technicians, ensuring efficient vehicle maintenance and repair.",
+      "Implemented resource allocation strategies, reducing wait times and optimizing workflow processes.",
+      "Enforced strict quality control measures, ensuring vehicles met high service standards.",
+      "Prioritized customer needs, cultivating strong client relationships and boosting repeat business.",
+      "Implemented safety protocols, minimizing workplace accidents and ensuring a secure working environment.",
+    ],
+  },
+  {
+    title: "Instructor",
+    company_name: "Shri Bikaner Motor Driving School",
+    icon: bikaner,
+    iconBg: "#E6DEDD",
+    date: "November 2020 - May 2021",
+    points: [
+      "Delivered comprehensive training on automotive components, imparting knowledge about their functions and applications for both four-wheelers and heavy-duty vehicles.",
+      "Successfully managed administrative tasks, including scheduling, resource allocation, and student progress tracking.",
+      "Utilized state-of-the-art simulation setups to teach safe driving practices, enhancing students' vehicle handling skills and road safety awareness.",
+      "Developed and updated training materials to ensure relevancy and alignment with industry standards.",
+      "Fostered an interactive learning environment, encouraging active participation and providing personalized guidance for improved comprehension.",
+    ],
+  },
+
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "AI Image Generation App",
+    description:
+      "Explore the future of creativity with our AI Image Generation App! 🤖🖼️ Powered by the incredible DALL-E model, this MERN stack marvel lets you turn ideas into stunning visuals. Express your imagination and design in a whole new way.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "OpenAIs",
+        color: "white-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/gourang123/AI-Image-Generation-App",
+    live_link: "https://relaxed-praline-f5d8ea.netlify.app/",
+  },
+  {
+    name: "3D Website",
+    description:
+      " a Responsive 3D Portfolio Website! 🌐 Explore my work in a stunning 3D environment, all thanks to React and Three.js",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "three.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Netlify",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/gourang123/3D-Portfolio-Website",
+    live_link: "https://relaxed-praline-f5d8ea.netlify.app/",
+  },
+  {
+    name: "Threads App",
+    description:
+      "Threads App! Nextjs 13.4, MongoDB, Clerk authentication, and more. Deployed successfully on Vercel",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/gourang123/threads_app",
+    live_link: "https://threads-app-six.vercel.app/",
+  },
+];
+
+export { services, technologies, experiences, projects };
