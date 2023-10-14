ec3e3d65dc6cbf01f228a5f44e4b99a09948224a
gourang123 <gourangsharma1998@gmail.com>
2023-10-14 17:10:43 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
               Hire Me
             </Link>
             <Link
-              to="./assets/RESUME.pdf"
+              to="./src/assets/RESUME.pdf"
               target="_blank"
               download="Resume.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
