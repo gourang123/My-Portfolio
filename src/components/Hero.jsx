9071af60c4ce97987c78cbec39cd4f1dbf09d4e6
gourang123 <gourangsharma1998@gmail.com>
2024-02-04 22:11:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,8 +44,7 @@ const Hero = () => {
               Hire Me
             </Link>
             <a
-              href="/assets/RESUME.pdf"
-              download = "Resume.pdf"
+              href="/assets/RESUME.pdf" target="_blank" 
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
